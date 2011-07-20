68a1144428f510ac4179745c4defd77e451bc9c3
Cosmin Stejerean <cosmin@offbytwo.com>
2011-07-20 14:31:47 -0600

    upgraded to latest opencart version

--- modules/opencart/templates/admin/config.php
+++ modules/opencart/templates/admin/config.php
@@ -1,24 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://<%= hostname %>.compute-1.amazonaws.com/admin/');
-define('HTTP_CATALOG', 'http://<%= hostname %>.compute-1.amazonaws.com/');
-define('HTTP_IMAGE', 'http://<%= hostname %>.compute-1.amazonaws.com/image/');
+define('HTTP_SERVER', 'http://<%= hostname %>/admin/');
+define('HTTP_CATALOG', 'http://<%= hostname %>/');
+define('HTTP_IMAGE', 'http://<%= hostname %>/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://<%= hostname %>.compute-1.amazonaws.com/admin/');
-define('HTTPS_IMAGE', 'http://<%= hostname %>.compute-1.amazonaws.com/image/');
+define('HTTPS_SERVER', 'http://<%= hostname %>/admin/');
+define('HTTPS_IMAGE', 'http://<%= hostname %>/image/');
 
 // DIR
-define('DIR_APPLICATION', '<%= location %>/catalog/');
-define('DIR_SYSTEM', '<%= location %>/system/');
-define('DIR_DATABASE', '<%= location %>/system/database/');
-define('DIR_LANGUAGE', '<%= location %>/catalog/language/');
-define('DIR_TEMPLATE', '<%= location %>/catalog/view/theme/');
-define('DIR_CONFIG', '<%= location %>/system/config/');
-define('DIR_IMAGE', '<%= location %>/image/');
-define('DIR_CACHE', '<%= location %>/system/cache/');
-define('DIR_DOWNLOAD', '<%= location %>/download/');
-define('DIR_LOGS', '<%= location %>/system/logs/');
+define('DIR_APPLICATION', '/var/opencart/admin/');
+define('DIR_SYSTEM', '/var/opencart/system/');
+define('DIR_DATABASE', '/var/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/opencart/system/config/');
+define('DIR_IMAGE', '/var/opencart/image/');
+define('DIR_CACHE', '/var/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/opencart/download/');
+define('DIR_LOGS', '/var/opencart/system/logs/');
+define('DIR_CATALOG', '/var/opencart/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
--- modules/opencart/templates/config.php
+++ modules/opencart/templates/config.php
@@ -1,15 +1,25 @@
 <?php
+// HTTP
+define('HTTP_SERVER', 'http://<%= hostname %>/');
+define('HTTP_IMAGE', 'http://<%= hostname %>/image/');
+define('HTTP_ADMIN', 'http://<%= hostname %>/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://<%= hostname %>/');
+define('HTTPS_IMAGE', 'http://<%= hostname %>/image/');
+
 // DIR
-define('DIR_APPLICATION', '<%= location %>/catalog/');
-define('DIR_SYSTEM', '<%= location %>/system/');
-define('DIR_DATABASE', '<%= location %>/system/database/');
-define('DIR_LANGUAGE', '<%= location %>/catalog/language/');
-define('DIR_TEMPLATE', '<%= location %>/catalog/view/theme/');
-define('DIR_CONFIG', '<%= location %>/system/config/');
-define('DIR_IMAGE', '<%= location %>/image/');
-define('DIR_CACHE', '<%= location %>/system/cache/');
-define('DIR_DOWNLOAD', '<%= location %>/download/');
-define('DIR_LOGS', '<%= location %>/system/logs/');
+define('DIR_APPLICATION', '/var/opencart/catalog/');
+define('DIR_SYSTEM', '/var/opencart/system/');
+define('DIR_DATABASE', '/var/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/opencart/system/config/');
+define('DIR_IMAGE', '/var/opencart/image/');
+define('DIR_CACHE', '/var/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/opencart/download/');
+define('DIR_LOGS', '/var/opencart/system/logs/');
+
 
 // DB
 define('DB_DRIVER', 'mysql');
